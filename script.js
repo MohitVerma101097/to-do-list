1aa49ee950ddd6fd87face52dd2e5eebb59f1518
Your Name <mkv97.mkv@gmail.com>
2024-03-24 15:56:25 +0100

    commit

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const addBtn = document.querySelector('#addTodoBtn')
 const input = document.querySelector('#todoInput')
 const list = document.querySelector('#todoList')
 
+const apiUrl = 'https://crudcrud.com/api/c101a604aeae417eaef095d22ac289c0'
+
 const fetchData = async() => {
 try {
-    const request = await fetch('https://crudcrud.com/api/c101a604aeae417eaef095d22ac289c0');
+    const request = await fetch(apiUrl);
     result = request.json()
     return result
 } catch (error) {
@@ -13,20 +15,24 @@ try {
 }
 
 window.onload = fetchData;
+addBtn.addEventListener("click", async () => {
+    const toDoText = input.value.trim(); 
 
-addBtn.addEventListener("click", async ()=>{
-    const toDoText = input.ariaValueMax.trim()
+    if (toDoText) { 
+        const todo = { text: toDoText }; 
 
-    if(text){
-        const todo = {text: toDoText}
-    }
-
-    try {
-        await fetchData(), {
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: { 
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(todo),
+            });
             
+        } catch (error) {
+            console.error(error); 
         }
-        
-    } catch (error) {
-        
+        fetch
     }
-})
+});
